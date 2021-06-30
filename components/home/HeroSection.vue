beabbd369351f81b260d7435854e43fe3cd4df45
SourceAura <sourceaura.dev@gmail.com>
2021-06-30 00:50:55 -0500

    🌺
    🌺

--- components/home/HeroSection.vue
+++ components/home/HeroSection.vue
@@ -90,7 +90,7 @@
                 </template>
                 <template #text>
                   <p>
-                    ...
+                    Play on Playa ^_^
                   </p>
                 </template>
                 <template #interactions>
@@ -125,14 +125,9 @@
 </template>
 
 <script>
-import nowPlay from "../spotify/nowPlay.vue";
 export default {
   data() {
-    return {
-      components: {
-        nowPlay,
-      },
-    };
+    return {};
   },
   methods: {},
 };
