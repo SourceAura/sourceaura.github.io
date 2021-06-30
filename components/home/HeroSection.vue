86969cd69b41e2d289ad94984078641c9224d055
SourceAura <sourceaura.dev@gmail.com>
2021-06-30 16:07:42 -0500

    🌹

--- components/home/HeroSection.vue
+++ components/home/HeroSection.vue
@@ -86,21 +86,14 @@
                   <h3>Now Playing:</h3>
                 </template>
                 <template #img>
+                  <!-- card left -->
                   <img :src="$config.image" alt="" />
 
                   
                 </template>
                 <template #text>
-                  <v-sheet
-                    :color="`grey ${theme.isDark ? 'darken-2' : 'lighten-4'}`"
-                    class="pa-3"
-                  >
-                    <v-skeleton-loader
-                      class="mx-auto"
-                      max-width="300"
-                      type="card"
-                    ></v-skeleton-loader>
-                  </v-sheet>
+                  <!-- card right -->
+                  
                 </template>
                 <template #interactions>
                   <!-- Base Stack Icons -->
