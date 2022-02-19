9cc29328b0bef4b961211dafe70c2e7799cd0ab9
SourceAura <sourceaura.dev@gmail.com>
2022-02-19 01:40:12 -0600

    🧼

---
+++ components/Comment.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="flex space-x-3 w-full rounded-md border-1px border-l border-r border-candyPink bg-transparent py-4 px-4">
+  <div class="flex-shrink-0">
+    <UserAvatar class="h-10 w-10 rounded-full" :name="comment.name" :photoURL="comment.photoURL"/>
+  </div>
+  <div class="w-full">
+    <div class="text-xs flex justify-between flex-row">
+      <p class="font-medium text-caribGreen">{{ comment.name }}</p>
+      <span class="text-white font-medium">{{ posted }}</span>
+    </div>
+    <div class="mt-1 text-sm text-gray-300">
+      <p>{{ comment.text }}</p>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+import { formatDistance } from 'date-fns'
+
+export default {
+  props: {
+    comment: {
+      type: Object
+    }
+  },
+  computed: {
+    posted() {
+      return formatDistance(new Date(this.comment.created), new Date(), {addSuffix: true})
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/home/HeroSection.vue
@@ -0,0 +1,163 @@
+<template>
+  <div class="relative bg-transparent overflow-hidden">
+    <div class="relative">
+      <main class="">
+        <div class="mx-auto py-8 max-w-7xl">
+          <div data-aos="zoom-in" class="lg:grid lg:grid-cols-12">
+            <div
+              class="px-4 sm:px-6 lg:border-r lg:border-dashed lg:border-laravel sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left lg:flex lg:items-center"
+            >
+              <div>
+                <!-- the chip -->
+                <nuxt-link
+                  v-if="$config.petals.enabled"
+                  :to="localePath('/petals')"
+                  class="group inline-flex hover:bg-black items-center text-white bg-transparent rounded-full p-0.5 pr-2 sm:text-base lg:text-sm xl:text-base hover:text-caribGreen"
+                >
+                  <span
+                    class="px-3 py-0.5 text-white text-xs font-semibold leading-5 tracking-wide bg-kingFish rounded-full"
+                    >{{ $t("hero.namaste") }}</span
+                  >
+                  <span class="ml-4 text-xs">{{ $t("hero.explore") }}</span>
+                  <!-- chip icon trans. -->
+                  <svg
+                    class="transition duration-500 transform block group-hover:rotate-180 hover-arrow ml-2 w-5 h-5 text-"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </nuxt-link>
+                <!--  chip end -->
+
+                <h1
+                  class="mt-4 text-3xl tracking-tight font-extrabold text-white sm:mt-5 sm:leading-none lg:mt-6 lg:text-3xl xl:text-3xl"
+                >
+                  <span class="md:block">{{ $t("hero.iAm") }}</span>
+                  <vue-typer :text="$t('hero.terms')"></vue-typer>
+                </h1>
+                <p class="mt-3 text-base text-white sm:mt-5">
+                  {{ $t("hero.aboutMe") }}
+                </p>
+                <!-- <p
+                  v-if="$config.workedAt.enabled"
+                  class="mt-8 text-sm text-white lowercase tracking-wide font-semibold sm:mt-10"
+                >
+                  The Stack:
+                </p> -->
+                <br />
+                <br />
+                <div class="">
+                  <div>
+                    <img
+                      alt="Vue.js"
+                      src="https://img.shields.io/badge/Vue.js-4FC08D.svg?style=for-the-badge&logo=vue-dot-js&logoColor=white"
+                    />
+                  </div>
+                  <div>
+                    <img
+                      alt="Nuxt.js"
+                      src="https://img.shields.io/badge/Nuxt.js-00C58E.svg?style=for-the-badge&logo=nuxt-dot-js&logoColor=white"
+                    />
+                  </div>
+                  <div>
+                    <img
+                      alt="Tailwind.css"
+                      src="https://img.shields.io/badge/Tailwind%20CSS-38B2AC.svg?style=for-the-badge&logo=Tailwind-CSS&logoColor=white"
+                    />
+                  </div>
+                </div>
+                <!--  -->
+               
+                <!--  -->
+              </div>
+            </div>
+
+            <!-- Hero Card  -->
+            <div class="mt-8 px-5 sm:mt-16 lg:mt-0 lg:col-span-6 ">
+              <vs-card type="2">
+                <template #title>
+                  <h5 class="text-white hover:text-kingFish ">
+                   ...
+                  </h5>
+                </template>
+                <template #text>
+                  <vs-avatar>
+                    <img :src="$config.image" alt="" />
+                  </vs-avatar>
+                </template>
+
+                <!-- bg -->
+                <template #img>
+                  <img
+                    src="https://media1.tenor.com/images/fbd438a9f31f5d8022a8568128228f2d/tenor.gif?itemid=21007368"
+                    alt=""
+                  />
+                </template>
+                <template #interactions>
+                  <!-- <vs-button color="#fff" transparent icon>
+                    <i class="bx bx-heart"></i>
+                  </vs-button> -->
+
+                  <!-- the stack -->
+                  <!-- <box-icon type="logo" name="vuejs"></box-icon>
+                  <box-icon name="firebase" type="logo"></box-icon>
+                  <box-icon type="logo" name="nodejs"></box-icon>
+                  <box-icon type="logo" name="css3"></box-icon>
+                  <box-icon type="logo" name="javascript"></box-icon>
+                  <box-icon name="html5" type="logo"></box-icon>
+                  <box-icon name="python" type="logo"></box-icon>
+                  <box-icon name="terminal"></box-icon> -->
+                </template>
+              </vs-card>
+            </div>
+            <!-- Hero Card End -->
+          </div>
+        </div>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeTooltip1: false,
+      components: {
+      },
+    };
+  },
+  inject: {
+    theme: {
+      default: { isDark: false },
+    },
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+>>> .vue-typer .custom.char {
+  @apply text-kingFish;
+}
+>>> .vue-typer .custom.char.selected {
+  @apply bg-kingFish text-white;
+}
+
+>>> .vue-typer .custom.caret {
+  width: 5px;
+  @apply bg-kingFish;
+}
+
+.hover-arrow {
+  position: relative;
+  stroke-width: 1px;
+}
+</style>
