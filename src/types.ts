597dd4ce8afe47e0adc220b99cf26afd3a79a276
SourceAura <sourceaura.dev@gmail.com>
2024-07-03 00:54:53 -0500

    hmm

---
+++ src/types.ts
@@ -0,0 +1,19 @@
+export type Site = {
+  ALIAS: string;
+  TITLE: string;
+  DESCRIPTION: string;
+  EMAIL: string;
+  NUM_POSTS_ON_HOMEPAGE: number;
+  NUM_PROJECTS_ON_HOMEPAGE: number;
+};
+
+export type Metadata = {
+
+  TITLE: string;
+  DESCRIPTION: string;
+};
+
+export type Socials = {
+  NAME: string;
+  HREF: string;
+}[];
