1df434483ba936b6740fcd4e9b040e988890ed14
SourceAura <sourceaura.dev@gmail.com>
2024-09-22 16:45:26 -0500

    ?

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from "@astrojs/sitemap";
 import pagefind from "astro-pagefind";
 import vue from '@astrojs/vue';
 import icon from "astro-icon";
-// import vercel from '@astrojs/vercel/serverless';
 import vercel from '@astrojs/vercel/serverless';
 
 import sentry from "@sentry/astro";
@@ -15,30 +14,26 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  // output: 'server',
-  adapter: vercel(),
-
+  output: 'hybrid',
+  adapter: vercel({
+    imageService: true,
+  }),
   site: "https://sourceaura.vercel.app",
   integrations: [tailwind(), icon(), sitemap(), mdx(), pagefind(), vue(), sentry(), spotlightjs(), react()],
-
   markdown: {
     shikiConfig: {
       theme: "css-variables"
     }
   },
-
   image: {
     service: {
       entrypoint: 'astro/assets/services/sharp'
     }
   },
-
   content: {
     collections: [
       'petals',
       // other collections if any
     ],
-  },
-
-  output: "server"
+  }
 });
