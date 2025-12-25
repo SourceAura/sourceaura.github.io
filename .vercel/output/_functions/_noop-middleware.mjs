3e8687a47cd5b7dfc81f27740f14e0419ae1cd60
SourceAura <sourceaura.dev@gmail.com>
2025-12-25 14:42:35 -0600

    .

---
+++ .vercel/output/_functions/_noop-middleware.mjs
@@ -0,0 +1,3 @@
+!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:{};e.SENTRY_RELEASE={id:"96e94530d97e603637f9db44419164a83c1e8fde"};}catch(e){}}();;{try{(function(){var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:{},n=(new e.Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="82e3c49e-9ee5-48d0-91bd-96d40e0d26d9",e._sentryDebugIdIdentifier="sentry-dbid-82e3c49e-9ee5-48d0-91bd-96d40e0d26d9");})();}catch(e){}};const onRequest = (_, next) => next();
+
+export { onRequest };
---
+++ .vercel/output/_functions/chunks/consts_Bca32Orw.mjs
@@ -0,0 +1,32 @@
+!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:{};e.SENTRY_RELEASE={id:"96e94530d97e603637f9db44419164a83c1e8fde"};}catch(e){}}();;{try{(function(){var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:{},n=(new e.Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="084221a0-64c8-422f-8ca2-1db92032ec29",e._sentryDebugIdIdentifier="sentry-dbid-084221a0-64c8-422f-8ca2-1db92032ec29");})();}catch(e){}};const VALID_INPUT_FORMATS = [
+  "jpeg",
+  "jpg",
+  "png",
+  "tiff",
+  "webp",
+  "gif",
+  "svg",
+  "avif"
+];
+const VALID_SUPPORTED_FORMATS = [
+  "jpeg",
+  "jpg",
+  "png",
+  "tiff",
+  "webp",
+  "gif",
+  "svg",
+  "avif"
+];
+const DEFAULT_OUTPUT_FORMAT = "webp";
+const DEFAULT_HASH_PROPS = [
+  "src",
+  "width",
+  "height",
+  "format",
+  "quality",
+  "fit",
+  "position"
+];
+
+export { DEFAULT_OUTPUT_FORMAT as D, VALID_INPUT_FORMATS as V, VALID_SUPPORTED_FORMATS as a, DEFAULT_HASH_PROPS as b };
